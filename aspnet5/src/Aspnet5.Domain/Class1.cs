0150a78d5c0de736576b5d0451d94c99398ddc9e
Alex Volnei Galante <alexg@N091261.local>
2020-08-27 23:32:47 -0300

    *

---
+++ aspnet5/src/Aspnet5.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Aspnet5.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ aspnet5/src/Aspnet5.Test/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Aspnet5.Test
+{
+    public class Class1
+    {
+    }
+}
